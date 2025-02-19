78b9cab2c40b7d96ecbab387e979f4d61eda4a25
NotArt <noterisarthur42@gmail.com>
2025-02-19 14:10:43 +0100

    + | CPP7 | Ex01 Iter

---
+++ CPP7/ex01/Iter.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * @param len is important, calling this template with a len not corresponding to the actual array length could cause Segfaults ;
+ */
+template <class T> void iter(T *array, int len, void f(T &elem)) {
+	for (int i = 0; i < len; i++) {
+		f(array[i]) ;
+	}
+}
---
+++ CPP7/ex01/main.cpp
@@ -0,0 +1,52 @@
+#include "Iter.hpp"
+#include <iostream>
+
+void times_two(int &i) {
+	i *= 2 ;
+}
+
+void str_trunc(std::string &str) {
+	if (str.size() > 3)
+		str.erase() ;
+}
+
+int main() {
+	int int_array[5] = {0, 1, 2, 3, 4} ;
+
+	for (size_t i = 0; i < 5; i++) {
+		std::cout << int_array[i] << " " ;
+	}
+	std::cout << std::endl ;
+
+	iter(int_array, 5, times_two) ;
+
+	for (size_t i = 0; i < 5; i++) {
+		std::cout << int_array[i] << " " ;
+	}
+	std::cout << std::endl ;
+
+	iter(int_array, 3, times_two) ;
+
+	for (size_t i = 0; i < 5; i++) {
+		std::cout << int_array[i] << " " ;
+	}
+	std::cout << std::endl ;
+
+	// ============================================== //
+
+	std::cout << std::endl ;
+
+	std::string str_array[5] = {"one", "two", "three", "four", "five"} ;
+
+	for (size_t i = 0; i < 5; i++) {
+		std::cout << str_array[i] << std::endl ;
+	}
+	std::cout << std::endl ;
+
+	iter(str_array, 5, str_trunc) ;
+
+	for (size_t i = 0; i < 5; i++) {
+		std::cout << str_array[i] << std::endl ;
+	}
+	std::cout << std::endl ;
+}
